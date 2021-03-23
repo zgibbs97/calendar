88141b70d762a04a32dbae40def4887b4ec2a623
zgibbs97 <zachary.gibbs15@gmail.com>
2021-03-22 20:03:14 -0700

    Finalized Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # calendar
+
+# For this project a client wanted a calendar to scheudle their day to day activities.
+
+# We we're givin a base code and asked to create a javascript and html. 
+
+# Using moment & local storage, we are able to save times and dates for the user.
+
+![Screenshot](calendar\img\unknown.png)
--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
-var timeDisplayEl = $('#time-display');
+var todayDate = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+$("#currentDay").html(todayDate);
 
 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-    console.log(displayTime);
-  }
+function hourlyTracker () {
+  var currentTime = moment().hour();
+
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1])
+    console.log(blockHour, currentTime)
+
+      if (blockHour < currentTime) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      }
+
+      else if (blockHour === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+
+      }
+
+      else {
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        $(this).removeClass("present");
+      }
+  })
+}
+hourlyTracker();
+
+$(".saveBtn").on('click', function (){
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr('id');
+
+  localStorage.setItem(time, text);
+})
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
